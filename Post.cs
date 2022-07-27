64eab5aef386aaaa0875f17e2417821f8a7c917d
Leandro Kuranaga <le.s.kuranaga@hotmail.com>
2022-07-27 17:46:32 -0300

    Add files via upload

---
+++ Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poc
+{
+    public class Post
+    {
+        public string tokenapi { get; set; }
+        public string idcontato { get; set; }
+        public string idresposta { get; set; }
+        public string ItemId { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = postData();
+
+        }
+
+        public static async Task postData() 
+        {
+            var url = "https://scx.sondait.com.br/webrun/WSMensagemEnviar.rule?sys=GB1";
+
+            HttpClient client = new HttpClient();
+
+            var body = new Dictionary<string, string> { { "tokenapi", "111B2E63-3DCE-4EC6-8CDB-D6BBB0A6DE13" }, { "idcontato", "leandro.silvak@sonda.com" }, { "idresposta", "consultar_chamado" }, { "ItemId", "7351" } };
+
+            var x = new Post
+            {
+                tokenapi = "111B2E63-3DCE-4EC6-8CDB-D6BBB0A6DE13",
+                idcontato = "leandro.silvak@sonda.com",
+                idresposta = "consultar_chamado",
+                ItemId = "7351"
+            };
+
+            var json = JsonConvert.SerializeObject(x);
+            var apiPostData = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var httpResponseMessage = client.PostAsync(url, apiPostData).Result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(httpResponseMessage);
+        }
+
+    }
+}
